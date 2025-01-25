e7d682b4f5fe82c5ed0786a2dc4597f009ee1d32
Ichaiwizman <ichaiv135@gmail.com>
2025-01-26 00:08:18 +0200

    ichai_V1

--- src/Components/helpers/NavBar.js
+++ src/Components/helpers/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import DrawerNavBAr from "./Drawer";
-// import DrawerCopy2 from "./DrawerCopy";
+import DrawerNavBAr from "./Drawer";
+import DrawerCopy2 from "./DrawerCopy";
 
 const NavBar = () => {
   const styles = {
@@ -53,6 +53,8 @@ const NavBar = () => {
       <Link to="/" style={styles.brandName}>
         Netanel Photography
       </Link>
+      <DrawerCopy2 />
+      <DrawerNavBAr />
     </div>
   );
 };
